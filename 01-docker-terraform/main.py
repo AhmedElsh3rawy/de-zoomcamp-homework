caa0d4cd5c9c1e144b6a19f95db4fa8543e3c0c0
Ahmed <ahmedelsharawy117@gmail.com>
2026-01-17 17:25:02 +0200

    feat: ingestion script

---
+++ 01-docker-terraform/main.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+from sqlalchemy import create_engine
+
+engine = create_engine("postgresql://postgres:password@localhost:5432/ny_taxi")
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64",
+}
+
+parse_dates = ["lpep_pickup_datetime", "lpep_dropoff_datetime"]
+
+df = pd.read_csv("green_tripdata_2025-11.csv", dtype=dtype, parse_dates=parse_dates)
+
+df.columns = (
+    df.columns.str.strip().str.lower().str.replace(" ", "_").str.replace("-", "_")
+)
+
+dtype_zones = {
+    "LocationId": "Int64",
+    "Borough": "string",
+    "Zone": "string",
+    "service_zone": "string",
+}
+
+df_zones = pd.read_csv("taxi_zone_lookup.csv", dtype=dtype_zones)
+
+df_zones.columns = (
+    df_zones.columns.str.strip().str.lower().str.replace(" ", "_").str.replace("-", "_")
+)
+
+table1 = "green_taxi_data"
+table2 = "taxi_zone_lookup"
+
+df.head(0).to_sql(table1, con=engine, if_exists="replace")
+
+df_zones.head(0).to_sql(table2, con=engine, if_exists="replace")
+
+df.to_sql(name=table1, con=engine, if_exists="append")
+df_zones.to_sql(name=table2, con=engine, if_exists="append")
