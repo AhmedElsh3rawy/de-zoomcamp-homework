e4f17fedde21b294c505b7211dcea72f28b36021
Ahmed <ahmedelsharawy117@gmail.com>
2026-01-18 11:03:31 +0200

    no need

--- 01-docker-terraform/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parquet
-
-wget https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv
-
